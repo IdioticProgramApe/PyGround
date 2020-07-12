09c6fde8ce9f9e942680d7518db2bf5a464c7c75
hanjiafeng <jiafeng_han@hotmail.com>
2020-07-12 22:17:46 +0800

    first commit to develop pyutil

---
+++ table.py
@@ -0,0 +1,122 @@
+from typing import Any, Iterable
+
+
+__version__ = "0.1.0"
+
+
+class Table(object):
+
+    # private variable
+    _sep = '='
+    _stringstream = ''
+    _rows = []
+    _name = ''
+    _alignment = '>'
+    _fformat = 'f'
+    
+
+    def __init__(self, title: str = None, max_width: int = 6, border: bool = False) -> None:
+        super(Table, self).__init__()
+
+        self.border = border
+        self._max_width = max_width
+        if title:
+            self._append(title)
+
+    def _append(self, strline: str, end: str = '\n') -> None:
+        self._stringstream += strline
+        self._stringstream += end
+
+    def _newl(self) -> None:
+        self._stringstream += '\n'
+
+    def add_rows(self, iterable: Iterable[str], sep: str = ' ') -> None:
+        if self.border:
+            # TODO
+            raise NotImplementedError("Current Table doesn't support border")
+        else:
+            for it in iterable:
+                self._rows.append(it)
+
+            self.add_line(sep=sep, end='\t')
+            self._stringstream += self._name
+            self._newl()
+            self._add_separation(sep)
+    
+    def add_line(self, iterable: Iterable[Any] = None, sep: str = ' ', end: str = '\n') -> None:
+        if iterable is None:
+            iterable = self._rows
+        
+        assert len(iterable) == len(self._rows), "Inputs length is incompatible with head info"
+
+        line_string = []
+        for it in iterable:
+            if isinstance(it, str):
+                line_string.append(f"{it:{self._alignment}{self._max_width}}")
+            elif isinstance(it, int):
+                line_string.append(f"{it:{self._alignment}{self._max_width}d}")
+            elif isinstance(it, float):
+                line_string.append(f"{it:{self._alignment}{self._max_width}.2{self._fformat}}")
+            else:
+                raise NotImplementedError(f"{type(it)} not recognizeable")
+        
+        strline = sep.join(line_string)
+        self._append(strline, end)
+
+    def _add_separation(self, sep: str):
+        self._append(self._sep * ((len(self._rows) - 1) * (self._max_width + len(sep)) + self._max_width))
+
+    def set_name(self, name: str):
+        self._name = name
+
+    def set_width(self, width: int):
+        self._max_width = width
+
+    def set_alignment(self, opt: str):
+        if opt == 'r':
+            self._alignment = '>'
+        elif opt == 'l':
+            self._alignment = '<'
+        elif opt == 'c':
+            self._alignment = '^'
+        else:
+            raise RuntimeError("Unknown Alignment Method, choose from ['r', 'l', 'c']")
+
+    def set_float_format(self, _format: str):
+        self._fformat = _format
+    
+    def clean(self):
+        self._stringstream = ''
+        self._rows = []
+        self._name = ''
+
+    def __repr__(self) -> str:
+        return self._stringstream
+    
+
+if __name__ == "__main__":
+    # create a table
+    table = Table()
+    table.set_name("testname.fbx")
+    table.set_alignment('r')  # set alignment to 'right'
+    table.add_rows(["", "a", "b"])
+    table.add_line(["c", 5151, 776])
+    table.add_line(["d", 0.654646, 0.87989416])
+    print("table is below")
+    print(table)
+
+    # clean the table to fill in new data
+    table.clean()
+    print("table is below")
+    print(table)
+
+    # new table
+    table.set_alignment('c')  # set alignment to 'center'
+    table.set_float_format('%')  # this time use percentage
+    table.set_width(7)  # since float has extra "%" char, we add one more place to width
+    table.add_rows(["", "a", "b"])
+    table.add_line(["c", 5151, 776])
+    table.add_line(["d", 0.654646, 1.0])
+    print("table is below")
+    print(table)
+    
