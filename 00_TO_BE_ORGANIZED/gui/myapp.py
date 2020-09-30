246aba1873af25b98964450cb2a13f0aa599de9c
hanjiafeng <hanjiafeng2398@virtuos.com.cn>
2020-09-30 18:49:18 +0800

    0 update myapp.py

--- 00_TO_BE_ORGANIZED/gui/myapp.py
+++ 00_TO_BE_ORGANIZED/gui/myapp.py
@@ -3,6 +3,7 @@ import sys
 from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
 from PySide2.QtCore import QSize, Qt
 
+
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
@@ -10,10 +11,19 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
         
         button = QPushButton("Press Me")
+        button.setCheckable(True)
+        button.clicked.connect(self.is_clicked)
+        button.clicked.connect(self.is_toggled)
         
         self.setFixedSize(QSize(400, 300))
         
         self.setCentralWidget(button)
+        
+    def is_clicked(self):
+        print("clicked")
+        
+    def is_toggled(self, checked: bool):
+        print(f"Checked?: {checked}")
 
 
 app = QApplication(sys.argv)
